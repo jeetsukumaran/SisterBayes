6a9f4aff2963100ec80fb007d671c2022514fcfd
Jeet Sukumaran <jeetsukumaran@gmail.com>
2018-01-03 09:19:37 -0500

    checked with floats

--- bin/sisterbayes-summarize.py
+++ bin/sisterbayes-summarize.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 import math
+import bisect
 import csv
 import os
 import sys
@@ -26,6 +27,83 @@ class SisterBayesSummarizer(object):
         self.stat_values = []
         self.other_values = []
 
+    def cluster_by_absolute_difference_threshold(self,
+            sp_labels,
+            realized_div_time_samples,
+            absolute_difference_threshold):
+        model_counter = collections.Counter()
+        for row_idx, realized_div_time_sample in enumerate(realized_div_time_samples):
+            div_time_model_desc = [None for i in sp_labels]
+            group_idx = 1
+            for sp_idx, sp_label in enumerate(sp_labels):
+                if sp_idx == 0:
+                    div_time_model_desc[sp_idx] = str(group_idx)
+                    group_idx += 1
+                else:
+                    current_dt = realized_div_time_sample[sp_label]
+                    for prev_sp_idx, prev_sp_label in enumerate(sp_labels[:sp_idx-1]):
+                        ref_dt = realized_div_time_sample[prev_sp_label]
+                        if abs(current_dt - ref_dt) <= (absolute_difference_threshold):
+                            div_time_model_desc[sp_idx] = div_time_model_desc[prev_sp_idx]
+                            break
+                    else:
+                        div_time_model_desc[sp_idx] = str(group_idx)
+                        group_idx += 1
+            model_name = "M"+"".join(div_time_model_desc)
+            try:
+                model_counter[model_name] += 1
+            except KeyError:
+                model_counter[model_name] = 1
+        return model_counter
+
+    # post-hoc derivation of div time model, clustering species that have
+    # population splits +/- t time units from each other.
+    def cluster_by_relative_difference_threshold(self,
+            sp_labels,
+            realized_div_time_samples,
+            all_div_times,
+            relative_difference_threshold):
+        v0 = min(all_div_times)
+        v1 = max(all_div_times)
+        absolute_difference_threshold = (v1 - v0) * relative_difference_threshold
+        return self.cluster_by_absolute_difference_threshold(
+                sp_labels=sp_labels,
+                realized_div_time_samples=realized_div_time_samples,
+                absolute_difference_threshold=absolute_difference_threshold)
+
+    def cluster_by_bin_size(self,
+            sp_labels,
+            realized_div_time_samples,
+            all_div_times,
+            bin_size):
+        bin_size = float(bin_size)
+        model_counter = collections.Counter()
+        for row_idx, realized_div_time_sample in enumerate(realized_div_time_samples):
+            div_time_model_desc = [None for i in sp_labels]
+            group_idx = 1
+            assigned_bin_idxs = set()
+            for sp_idx, sp_label in enumerate(sp_labels):
+                current_dt = realized_div_time_sample[sp_label]
+                assigned_bin_idx = current_dt / bin_size
+                if sp_idx == 0:
+                    div_time_model_desc[sp_idx] = str(group_idx)
+                    group_idx += 1
+                else:
+                    for prev_sp_idx, prev_sp_label in enumerate(sp_labels[:sp_idx-1]):
+                        ref_dt = realized_div_time_sample[prev_sp_label]
+                        if abs(current_dt - ref_dt) <= (absolute_difference_threshold):
+                            div_time_model_desc[sp_idx] = div_time_model_desc[prev_sp_idx]
+                            break
+                    else:
+                        div_time_model_desc[sp_idx] = str(group_idx)
+                        group_idx += 1
+            model_name = "M"+"".join(div_time_model_desc)
+            try:
+                model_counter[model_name] += 1
+            except KeyError:
+                model_counter[model_name] = 1
+        return model_counter
+
     def summarize(self, target_data_filepath,):
         with utility.universal_open(target_data_filepath) as src:
             reader = csv.DictReader(
@@ -34,11 +112,11 @@ class SisterBayesSummarizer(object):
                     quoting=csv.QUOTE_NONE)
             categorical_params = collections.OrderedDict()
             continuous_params = collections.OrderedDict()
-            realized_div_time_regimes = []
+            realized_div_time_samples = []
             all_div_times = []
             sp_labels = []
             for row_idx, row in enumerate(reader):
-                realized_div_time_regimes.append({})
+                realized_div_time_samples.append({})
                 for key_idx, key in enumerate(reader.fieldnames):
                     if key in categorical_params:
                         categorical_params[key][row[key]] += 1
@@ -62,36 +140,18 @@ class SisterBayesSummarizer(object):
                             continuous_params[key] = [val]
                     if key.startswith("param.divTime."):
                         sp_label = key.replace("param.divTime.", "")
-                        realized_div_time_regimes[-1][sp_label] = continuous_params[key][-1]
+                        realized_div_time_samples[-1][sp_label] = continuous_params[key][-1]
                         all_div_times.append(val)
                         if row_idx == 0:
                             sp_labels.append(sp_label)
             ### EXPERIMENTAL ###
-            categorical_params["param.effectiveDivTimeModel"] = collections.Counter()
-            threshold = max(all_div_times) * 0.01
-            for row_idx, realized_div_time_regime in enumerate(realized_div_time_regimes):
-                div_time_model_desc = [None for i in sp_labels]
-                group_idx = 1
-                for sp_idx, sp_label in enumerate(sp_labels):
-                    if sp_idx == 0:
-                        div_time_model_desc[sp_idx] = str(group_idx)
-                        group_idx += 1
-                    else:
-                        current_dt = realized_div_time_regime[sp_label]
-                        for prev_sp_idx, prev_sp_label in enumerate(sp_labels[:sp_idx-1]):
-                            ref_dt = realized_div_time_regime[prev_sp_label]
-                            if abs(current_dt - ref_dt) <= (threshold):
-                                div_time_model_desc[sp_idx] = div_time_model_desc[prev_sp_idx]
-                                break
-                        else:
-                            div_time_model_desc[sp_idx] = str(group_idx)
-                            group_idx += 1
-                model_name = "M"+"".join(div_time_model_desc)
-                try:
-                    categorical_params["param.effectiveDivTimeModel"][model_name] += 1
-                except KeyError:
-                    categorical_params["param.effectiveDivTimeModel"][model_name] = 1
+            categorical_params["param.effectiveDivTimeModel"] = self.cluster_by_relative_difference_threshold(
+                    sp_labels=sp_labels,
+                    realized_div_time_samples=realized_div_time_samples,
+                    all_div_times=all_div_times,
+                    relative_difference_threshold=0.01)
             ### EXPERIMENTAL ###
+
             output_prefix = os.path.splitext(os.path.basename(target_data_filepath))[0]
             with utility.universal_open(output_prefix + ".summary.continuous.tsv", "w") as dest:
                 row_results = collections.OrderedDict()
--- sisterbayes/test/test_binning.py
+++ sisterbayes/test/test_binning.py
@@ -8,12 +8,18 @@ class BisectTestCase(unittest.TestCase):
 
     def validateBinIndex(self, value, bin_size):
         obs = utility.bin_index(value, bin_size)
-        check_array = [i for i in range(0, value * 2, bin_size)]
+        factor = 1e6
+        check_array = [i/float(factor) for i in range(0, int(value * 2 * factor), int(bin_size * factor))]
         self.assertEqual(bisect.bisect_left(check_array, value), obs)
 
-    def test1(self):
+    def test_int(self):
         for value in [3, 4, 7, 9, 17, 1, 0, 15, 2012, 23, 1, 83]:
             for bin_size in [1,2,3,4,5,7,11,13,100]:
+                self.validateBinIndex(value, bin_size)
+
+    def test_float(self):
+        for value in [3.14, 0.04, 0.12, 9.112, 0.0017, 0.00511, 0.12, 15.173741, 2.18182, 0.123, 0.101, 0.00283]:
+            for bin_size in [0.1, 0.001, 0.5, 1.0, 0.015, 0.13, 0.00001]:
                 self.validateBinIndex(value, bin_size)
 
 if __name__ == "__main__":
